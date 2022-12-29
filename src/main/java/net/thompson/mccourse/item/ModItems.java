9a82a7bc871282579ccfa45a51d1d676fa3e2b47
Kelsey Thompson <slipknotxsx@hotmail.com>
2022-12-29 15:40:47 -0500

    50. Creating a new armor set
    Added: Cobalt boots, chestplate, helmet, leggings

--- src/main/java/net/thompson/mccourse/item/ModItems.java
+++ src/main/java/net/thompson/mccourse/item/ModItems.java
@@ -1,5 +1,6 @@
 package net.thompson.mccourse.item;
 
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.RegistryObject;
@@ -57,6 +58,23 @@ public class ModItems {
 
     public static final RegistryObject<Item> COBALT_PAXEL = ITEMS.register("cobalt_paxel",
             () -> new PaxelItem(ModTiers.COBALT, 0, 0f,
+                    new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+
+    public static final RegistryObject<Item> COBALT_HELMET = ITEMS.register("cobalt_helmet",
+            () -> new ArmorItem(ModArmorMaterials.COBALT, EquipmentSlot.HEAD,
+                    new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_CHESTPLATE = ITEMS.register("cobalt_chestplate",
+            () -> new ArmorItem(ModArmorMaterials.COBALT, EquipmentSlot.CHEST,
+                    new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_LEGGINGS = ITEMS.register("cobalt_leggings",
+            () -> new ArmorItem(ModArmorMaterials.COBALT, EquipmentSlot.LEGS,
+                    new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_BOOTS = ITEMS.register("cobalt_boots",
+            () -> new ArmorItem(ModArmorMaterials.COBALT, EquipmentSlot.FEET,
                     new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
 
 
